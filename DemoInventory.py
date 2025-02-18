eeb4cd0879260cabfea0233c4b42cb02b77c5ea3
Liznel Grimaldo <108779338+Vansik4@users.noreply.github.com>
2025-02-17 19:57:10 -0500

    Update DemoInventory.py

--- DemoInventory.py
+++ DemoInventory.py
@@ -10,7 +10,7 @@ from google.cloud import documentai
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcs_connections"]
+    st.secrets["gcs_connections"])
 
 client = documentai.DocumentProcessorServiceClient(credentials=credentials)
 
